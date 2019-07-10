1ef0796184e50dbc9bff50e37240c60ce5056603
Claudio G. Ametrano <42776679+claudioametrano@users.noreply.github.com>
2019-07-10 10:15:41 +0200

    Update resample_random_alignment_columns_from_fasta_FINAL.py

--- resample_random_alignment_columns_from_fasta_FINAL.py
+++ resample_random_alignment_columns_from_fasta_FINAL.py
@@ -12,67 +12,65 @@ header_list = []
 the_matrix = []
 resampled_matrix = []
 count = 0
-alignment_file = input("Inserire il nome del file fasta da ricampionare:")
-rasampling_repeats = input("Quanti alignment ricampionati vuoi?: ")
+alignment_file = input("File name to be resampled:")
+rasampling_repeats = input("How many alignments do you need?: ")
 rasampling_repeats = int(rasampling_repeats)
 for record in SeqIO.parse(alignment_file, "fasta"):
-#SeqInputOutput.parse legge i dati del fasta (con "fasta" specifichi in che formato è il file con le sequenze)	
+#SeqInputOutput.parse reads the fasta file	
 	header_list.append(record.id)
-#record.id sono gli header (nel pacchetto SeqIO di Biopython), record.seq sono invece le sequenze	
+#record.id are the headers (in SeqIO of Biopython), record.seq are the sequences	
 #print(header_list)	
-# crea una lista appendendo uno dopo l'altro gli header delle sequenze	
+# creates a list of the headers	
 number_of_sequences = len(header_list)
-# conta gli elementi della lista creata
+# counts the element of the list
 print("il numero delle sequenze è %s" %(number_of_sequences))
 sequences_length = len(record.seq)
-# conta i caratteri di una sequenza... si suppone le sequenze siano tutte lunghe uguali (dopo averle allineate e tagliate)
-print("l'alignment è lungo %s nucleotidi" %(sequences_length))
-columns_to_sample = int(input("Quante posizioni vuoi campionare?"))
+# counts the nucleotides of the alignment
+print(" the alignment is %s nucleotided" %(sequences_length))
+columns_to_sample = int(input("How many alignment posotions do you want to sample?"))
 # lo legger come un integer
         
 for record in SeqIO.parse(alignment_file, "fasta"):
 		a = record.seq
 		split_a = list(record.seq)
 		the_matrix.append(split_a)
-#prendo le requenze una per una ricorsivamente e crea per ognuna una lista fatta dai singoli nucleotidi che appendo alla lista the_matrix		
-#print("la lista di liste contententi le sequenze è:")
+#creates a list of the nucleotides for every sequence in the alignment and appends them to the list the_matrix		
+#print("The list of lists is:")
 #print(the_matrix)
 
-#print("eccola rappresentata come righe e colonne")
+#print("the same list represented as row and columns")
 #for righe in the_matrix:
 #	for oggetto in righe:
 #		print (oggetto, end='')
 #	print()
-# stampa la lista bella ordinata :D
+# it print the list in a nice, tidy up way
 
 the_matrix_numpy = np.array(the_matrix) 
-#trasforma la lista di liste in un array a due dimensioni 
-#print("ecco la lista di lista trasformata in array:") 
+#the list of lists is converted in a two dimension array 
+#print("Here is the array:") 
 #print(the_matrix_numpy)
 
 for k in range (rasampling_repeats):
 	count = count + 1
 	number_list = list(range(sequences_length))
 	random_number_list = random.sample(number_list, columns_to_sample)   
-	print("posizioni campionate nell'alignment",random_number_list)
-	#crea lista di integer lunga come l'alignment, campiona a caso da questa n numeri e li mette in una lista (senza ripetizione)
-	
-	"""#SE VOGLIO CAMPIONARE MA CON RIPETIZIONE...
+	print("sampled position in the alignment",random_number_list)
+	#creates a list of integer with the same length of the alignment, then samples randomply n numbers and put them in a list (without repetition)
+	"""#If I want to sample with repetition...
 	random_number_list = []
-	#creo un lista vuota
+	#creates an empty list
 	for i in range(0,5):
-	#campioni 5 numeri	
+	#samples for instance 5 numbers	
 		random_number_list.append(random.randrange(0,14))                       
 	print(random_number_list)
-	#appendere alla lista un certo numero n (range(0,n)) di numeri random tra 0 e m (random.ranrange(0,m))
-	#mentra random.randrange crea solo i numeri casuali col range che gli indichi e poi li devi appendere tu ad una lista
-	#il comando random.sample campiona da una lista già riempita di numeri e crea una lista coi numeri campionati random"""
-
-	resampled_matrix = the_matrix_numpy[:,random_number_list]
-	print("la matrice che rappresenta l'alignment ricampionato è")
-	print(resampled_matrix)
-	# la nuova matrice sarà composta dalle colonne identificate dai numeri casuali generati nella "random_number_list"
+	#appends to the list a certain number of random numbers between 0 and m (random.ranrange(0,m))
+	#while random.randrange only creates random numbers using the range and then you have to append them to a list
+	# the command random.sample samples from an alreay existing list of numbers and creates a list with the sampled numbers """
 	
+	resampled_matrix = the_matrix_numpy[:,random_number_list]
+	print("the resampled alignment is: ")
+	print(resampled_matrix)
+	# the new matrix is composed by the columns wchich correspond the the random number generated in the "random_number_list"
 	output_file = open("resampled_alignment", "w")
 	percentage = int(columns_to_sample) / int(sequences_length)*100 
 	percentage = round(percentage, 1)
@@ -83,11 +81,11 @@ for k in range (rasampling_repeats):
 	os.rename(path +"/"+ "resampled_alignment", path +"/"+percentage + "%" + "resampled_alignment" + count + ".fas")
 	count = int(count)
 	for header, row in zip(header_list, resampled_matrix):
-	# zip is useful to run two different for cycle in a parallel way!:)	
-	# questo ciclo for scrive in modo alternato l'headr e la sua sequenza
+	# zip is useful when you have to run two for cycle in a paralle way
+	#this for cycle writes in a parallel way the header and its sequence
 		output_file.write(">" + header + "\n")	
 		line= (''.join([str(elem) for elem in row]))	
 	# the resampled sequence is a join, without spaces of the elements in a row	
 		output_file.write(line + "\n")
 
-	print("A partire dall'alignment file ", alignment_file, "sono stati creati ",count,"alignment campionando", columns_to_sample, "posizioni senza ripetizione, gli output sono salvati nei file resampled_alignment.fas")
+	print("From the aligment file: ", alignment_file, "have been created ",count,"alignments, samplig", columns_to_sample, "alignment position without repetition, output have been saved to resampled_alignment.fas")
